273cb937d05e2fee8374853a61ab90c7a489a5ea
Tobani <tobanigeorge@gmail.com>
2024-10-05 17:16:25 +0100

    add(page): add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import { Button, Typography } from "@mui/joy";
+import Link from "next/link";
+
 export default async function Home() {
-  return <>Hello world</>;
+  return (
+    <main>
+      <section className="flex h-screen flex-col items-center justify-center">
+        <Typography level="h1" sx={{ color: "primary.500" }}>
+          InvoiceStack
+        </Typography>
+        <Typography level="title-lg">Simplified Invoice Payments</Typography>
+        <div className="mt-4 flex gap-3">
+          <Link href="/invoices/new">
+            <Button>Get Started</Button>
+          </Link>
+          <Link href="/invoices/list">
+            <Button color="neutral" variant="outlined">
+              My Invoices
+            </Button>
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
 }
